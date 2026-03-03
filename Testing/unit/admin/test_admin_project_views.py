bad724f264862c6922da6029e3c23a6bc8c79269
chrys <zampas@gmail.com>
2026-03-03 17:45:57 +0000

    test(admin): Write failing test stubs for project views

---
+++ Testing/unit/admin/test_admin_project_views.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.mark.django_db
+class TestAdminProjectViews:
+    def test_create_project_google(self):
+        assert False, "test_create_project_google not implemented"
+
+    def test_create_project_postgres(self):
+        assert False, "test_create_project_postgres not implemented"
+
+    def test_delete_project_google(self):
+        assert False, "test_delete_project_google not implemented"
+
+    def test_delete_project_postgres(self):
+        assert False, "test_delete_project_postgres not implemented"
