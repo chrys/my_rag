075c492e32a6d34d8ccb32d73488d4c28cebc726
chrys <zampas@gmail.com>
2026-03-04 15:01:41 +0000

    test(admin): Write failing test stubs for document views

---
+++ Testing/unit/admin/test_admin_document_views.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.mark.django_db
+class TestAdminDocumentViews:
+    def test_upload_document_google(self):
+        assert False, "test_upload_document_google not implemented"
+
+    def test_upload_document_postgres(self):
+        assert False, "test_upload_document_postgres not implemented"
+
+    def test_delete_document_google(self):
+        assert False, "test_delete_document_google not implemented"
+
+    def test_delete_document_postgres(self):
+        assert False, "test_delete_document_postgres not implemented"
