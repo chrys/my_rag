2eeb0ccb669dec1bc1700a7268814698f06b9ea8
chrys <zampas@gmail.com>
2026-03-04 15:12:47 +0000

    test(chat): Write failing test stubs for rag chat llm

---
+++ Testing/unit/chat/test_chat_rag_llm.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.django_db
+class TestChatRagLLM:
+    def test_chat_rag_related_answer(self):
+        assert False, "test_chat_rag_related_answer not implemented"
+
+    def test_chat_rag_unrelated_answer(self):
+        assert False, "test_chat_rag_unrelated_answer not implemented"
