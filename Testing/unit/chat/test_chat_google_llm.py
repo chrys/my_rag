65f3c04720226cbea45a1209529663f638644eb3
chrys <zampas@gmail.com>
2026-03-04 15:10:37 +0000

    test(chat): Write failing test stubs for google chat llm

---
+++ Testing/unit/chat/test_chat_google_llm.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.django_db
+class TestChatGoogleLLM:
+    def test_chat_google_related_answer(self):
+        assert False, "test_chat_google_related_answer not implemented"
+
+    def test_chat_google_unrelated_answer(self):
+        assert False, "test_chat_google_unrelated_answer not implemented"
