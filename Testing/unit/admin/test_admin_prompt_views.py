4972fb8e06c52f69d9e93a3c219b4638293703a1
chrys <zampas@gmail.com>
2026-03-04 15:07:09 +0000

    test(admin): Write failing test stubs for custom prompt views

---
+++ Testing/unit/admin/test_admin_prompt_views.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.django_db
+class TestAdminPromptViews:
+    def test_add_custom_prompt(self):
+        assert False, "test_add_custom_prompt not implemented"
+
+    def test_edit_custom_prompt(self):
+        assert False, "test_edit_custom_prompt not implemented"
+
+    def test_system_prompt_passed_to_llm(self):
+        assert False, "test_system_prompt_passed_to_llm not implemented"
