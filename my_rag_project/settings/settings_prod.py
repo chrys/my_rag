10bbb5c1880b736cabeae1c264d21eca197d1a89
chrys <zampas@gmail.com>
2026-04-29 13:25:06 +0100

    more logs

--- my_rag_project/settings/settings_prod.py
+++ my_rag_project/settings/settings_prod.py
@@ -28,8 +28,15 @@ CORS_ALLOWED_ORIGINS = [
     'https://fasolaki.com',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://www.fasolaki.com',
+    'https://fasolaki.com',
+]
+
 # Security settings for production
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
