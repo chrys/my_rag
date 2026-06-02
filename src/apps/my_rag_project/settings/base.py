8139e0b4e2f0301139937467820b547d591aa178
chrys <zampas@gmail.com>
2026-06-02 10:36:07 +0100

    unfold styles

--- src/apps/my_rag_project/settings/base.py
+++ src/apps/my_rag_project/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # CORS middleware - must be before common
     'django.middleware.common.CommonMiddleware',
